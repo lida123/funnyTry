f30a8afbfdecc42fc9f3d71a930e957c35228243
petyou <812607796@qq.com>
2017-10-31 17:20:33 +0800

    初始化项目

---
+++ funnyTry/funnyTry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  funnyTry
+//
+//  Created by SGQ on 2017/10/31.
+//  Copyright © 2017年 GQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
