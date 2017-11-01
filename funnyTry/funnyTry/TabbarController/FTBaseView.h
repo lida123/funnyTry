1b0aa35f56e4acba8652c627c5213338c1aff3e5
petyou <812607796@qq.com>
2017-11-01 14:05:54 +0800

    Revert "删除baseview"
    This reverts commit fb8576ba057aea4ade1c53e1c9876683d3cc6d1f.
    
    恢复baseView

---
+++ funnyTry/funnyTry/TabbarController/FTBaseView.h
@@ -0,0 +1,13 @@
+//
+//  FTBaseView.h
+//  funnyTry
+//
+//  Created by SGQ on 2017/11/1.
+//  Copyright © 2017年 GQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTBaseView : UIView
+
+@end
