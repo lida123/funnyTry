6b3e939d4bb656f00f40149caa153f977ef336bf
petyou <812607796@qq.com>
2017-11-01 13:55:21 +0800

    add baseView

---
+++ funnyTry/funnyTry/TabbarController/FTBaseView.m
@@ -0,0 +1,21 @@
+//
+//  FTBaseView.m
+//  funnyTry
+//
+//  Created by SGQ on 2017/11/1.
+//  Copyright © 2017年 GQ. All rights reserved.
+//
+
+#import "FTBaseView.h"
+
+@implementation FTBaseView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
