7d73eb3ff019bcacf01da892aa768c1c15a6a9da
petyou <812607796@qq.com>
2017-11-03 14:13:38 +0800

    meue ajustment

---
+++ funnyTry/funnyTry/Class/BaseUI/FTBaseTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FTBaseTableViewController.h
+//  funnyTry
+//
+//  Created by SGQ on 2017/11/3.
+//  Copyright © 2017年 GQ. All rights reserved.
+//
+
+#import "FTBaseViewController.h"
+
+@interface FTBaseTableViewController : FTBaseViewController
+
+@end
---
+++ funnyTry/funnyTry/Class/BaseUI/FTBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  FTBaseViewController.h
+//  funnyTry
+//
+//  Created by SGQ on 2017/11/3.
+//  Copyright © 2017年 GQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTBaseViewController : UIViewController
+
+@end
