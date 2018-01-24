e25a4a5c8b3e8e8c91c2691b8fe0b4c7ae3c9d26
petyou <812607796@qq.com>
2018-01-24 11:23:26 +0800

    提交遗漏文件

---
+++ funnyTry/funnyTry/Class/JustForFun/CornerRadius/FTCornerRadiusController.h
@@ -0,0 +1,13 @@
+//
+//  FTCornerRadiusController.h
+//  funnyTry
+//
+//  Created by SGQ on 2018/1/10.
+//  Copyright © 2018年 GQ. All rights reserved.
+//
+
+#import "FTBaseViewController.h"
+
+@interface FTCornerRadiusController : FTBaseViewController
+
+@end
